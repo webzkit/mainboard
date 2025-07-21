43b2fbaa3bb4000ebc2bec48b8e55d6b54aafac3
tqhoa <tqhoa8th@gmail.com>
2025-07-21 18:05:00 +0700

    add minio

--- observability/fastapi_app/main.py
+++ observability/fastapi_app/main.py
@@ -117,6 +117,4 @@ if __name__ == "__main__":
         "fmt"
     ] = "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s] - %(message)s"
 
-    uvicorn.run(
-        app, host="0.0.0.0", reload=True, port=EXPOSE_PORT, log_config=log_config
-    )
+    uvicorn.run(app, host="0.0.0.0", port=EXPOSE_PORT, log_config=log_config)
